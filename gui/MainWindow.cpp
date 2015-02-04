a27ddee0fd58ac49f336ce33d019ee8404e7e120
Petr Mrázek <peterix@gmail.com>
2015-02-04 21:10:10 +0100

    SCRATCH move things to the right places

--- gui/MainWindow.cpp
+++ gui/MainWindow.cpp
@@ -73,7 +73,7 @@
 
 #include "logic/InstanceList.h"
 #include "logic/minecraft/MinecraftVersionList.h"
-#include "logic/LwjglVersionList.h"
+#include "logic/minecraft/LwjglVersionList.h"
 #include "logic/icons/IconList.h"
 #include "logic/java/JavaVersionList.h"
 
@@ -89,18 +89,15 @@
 #include "logic/Env.h"
 
 #include "logic/BaseInstance.h"
-#include "logic/OneSixInstance.h"
 #include "logic/BaseProcess.h"
-#include "logic/OneSixUpdate.h"
 #include "logic/java/JavaUtils.h"
 #include "gui/NagUtils.h"
-#include "logic/SkinUtils.h"
+#include "logic/minecraft/SkinUtils.h"
 
-#include "logic/LegacyInstance.h"
+//#include "logic/minecraft/LegacyInstance.h"
 
-#include "logic/assets/AssetsUtils.h"
 #include <logic/updater/UpdateChecker.h>
-#include <logic/updater/NotificationChecker.h>
+#include <logic/notifications/NotificationChecker.h>
 #include <logic/tasks/ThreadTask.h>
 
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow)
